afb76f8f8591430630b1ed0675b3eb51ad5f0866
Kangkook Jee <aixer77@gmail.com>
2012-09-25 11:19:54 -0400

    cleaned up code a bit

--- hw1/replace.cpp
+++ hw1/replace.cpp
@@ -29,33 +29,33 @@ namespace {
         }
       }
 
-
-      for (Module::FunctionListType::iterator b = functions.begin(), be = functions.end(); b != be; ++b) {
-        for (Function::BasicBlockListType::iterator i = b->begin(), ie = b->end(); i != ie; ++i) {
-          for(BasicBlock::InstListType::iterator inst=i->begin(),inste=i->end();inst!=inste;inst++)
+      for (Module::FunctionListType::iterator b = functions.begin(), be = functions.end();
+           b != be;
+           ++b) {
+        for (Function::BasicBlockListType::iterator i = b->begin(), ie = b->end(); 
+             i != ie; 
+             ++i) {
+          for(BasicBlock::InstListType::iterator inst = i->begin(),
+                inste = i->end();
+              inst!=inste;
+              inst++)
           {
-
             //InvokeInst * invokeInst=NULL;
             if (CallInst*  callInst = dyn_cast<CallInst>(inst) ) {
-              errs()<<"find a call in ";
-              errs().write_escaped(b->getName())<<":";
-              errs().write_escaped(callInst->getCalledFunction()->getName())<<"\n";
+              errs() << "find a call in ";
+              errs().write_escaped(b->getName()) << ":";
+              errs().write_escaped(callInst->getCalledFunction()->getName()) << "\n";
+
               if(callInst->getCalledFunction()->getName()=="printf"){}
-              //callInst->setCalledFunction(func);
-            }//if
-
-          }//for Instruction
-        }
-      }//for module
-
-
-
+            }  /* if */
+          }  /* for instruction */
+        } /* for block */
+      }  /* for module */
       return true;
 
     }//runonmodule
-
-
   };//struct
 }//namespace
 char replace::ID = 0;
+
 static RegisterPass<replace> X("replace", "replacepass", false, false);
