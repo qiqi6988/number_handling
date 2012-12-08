ca0f475a9304999ba773d2d242a06a40f4f35d12
qiqi <mz2369@columbia.edu>
2012-12-07 23:13:04 -0500

    rm tmp file

--- TCs/TC_C_194_vx31/src/Bad_gcc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Bad for gcc"
-export QUERY_STRING="filename=2.java&length=90"
-echo "`./countlines`"
--- TCs/TC_C_195_vx32/src/runbad_ioc_notrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Bad"
-export QUERY_STRING="cmd=7+0+-10+../QKtest/Bad_ioc_notrap.txt"
-echo "`./cmdInterpreter_ioc_notrap`"
